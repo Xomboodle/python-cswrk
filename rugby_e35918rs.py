7886f2fe551c4af0833530e3c6e82720b6261fe3
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2021-11-09 17:13:29 +0000

    Mid_Term Exercise 1

--- rugby_e35918rs.py
+++ rugby_e35918rs.py
@@ -1,26 +1,35 @@
-import argparse
+import argparse, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input')
 parser.add_argument('output')
 args = parser.parse_args()
 
-with open(args.input,'r') as file1:
-	words = file1.read()
 
-scores = [0,0]
+files = os.listdir(args.input + "/")
+for i in files:
+	with open(args.input + "/" + i,'r') as f:
+		words = f.read()
 
-for i in range(1,len(words), 3):
-	if words[i+1] == 't': to_add = 5
-	elif words[i+1] == 'c': to_add = 2
-	elif words[i+1] == 'p': to_add = 3
-	elif words[i+1] == 'd': to_add = 3
+	scores = [0,0]
 
-	if words[i] == '1':
-		scores[0] += to_add
-	else:
-		scores[1] += to_add
+	for j in range(1,len(words), 3):
+		if words[j+1] == 't': to_add = 5
+		elif words[j+1] == 'c': to_add = 2
+		elif words[j+1] == 'p': to_add = 3
+		elif words[j+1] == 'd': to_add = 3
 
-out_file = open(args.output,'w')
-out_file.write(str(scores[0])+":"+str(scores[1]))
-out_file.close()
+		if words[j] == '1':
+			scores[0] += to_add
+		else:
+			scores[1] += to_add
+	try:
+		out_file = open(args.output + "/" + i[:len(i)-4] + "_e35918rs.txt",'w')
+		out_file.write(str(scores[0])+":"+str(scores[1]))
+		out_file.close()
+	except:
+		os.mkdir(args.output)
+		out_file = open(args.output + "/" + i[:len(i)-4] + "_e35918rs.txt",'w')
+		out_file.write(str(scores[0])+":"+str(scores[1]))
+		out_file.close()
+	print("SUCCESS")
