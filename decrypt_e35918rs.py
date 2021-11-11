00f7ff59ad5ce8ec854d188c3b3100ddf9c88a20
e35918rs <ryan.sullivan-4@student.manchester.ac.uk>
2021-11-11 00:00:12 +0000

    Mid-Term Exercise 2

--- decrypt_e35918rs.py
+++ decrypt_e35918rs.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input')
@@ -18,7 +18,6 @@ def decryptHex(code):
 
 def decryptCaesar(code):
 	alphabet = list("abcdefghijklmnopqrstuvwxyz")
-	print(alphabet)
 	decrypted = ""
 	for i in range(len(code)):
 		if code[i] not in alphabet:
@@ -33,9 +32,10 @@ def decryptMorse(code):
 					"--":"m","-.":"n","---":"o",".--.":"p","--.-":"q",".-.":"r",
 					"...":"s","-":"t","..-":"u","...-":"v",".--":"w","-..-":"x",
 					"-.--":"y","--..":"z",".-.-.-":".","--..--":",","..--..":"?",
-					"-..-.":"/","...-.-":"@",".----":"1","..---":"2","...--":"3",
+					"-..-.":"/","-.-.-.":";","---...":":","-....-":"-",".----.":"'",
+					"-.--.-":")","-.--.":"(",".----":"1","..---":"2","...--":"3",
 					"....-":"4",".....":"5","-....":"6","--...":"7","---..":"8",
-					"----.":"9","-----":"0"}
+					"----.":"9","-----":"0","-.-.--":"!"}
 
 	code = code.split(" ")
 	decrypted = ""
@@ -46,17 +46,36 @@ def decryptMorse(code):
 		decrypted += morse_dict.get(i)
 	return decrypted
 
+files = os.listdir(args.input)
+if args.input[-1] == "/":
+	path_start = args.input
+else:
+	path_start = args.input + "/"
+for i in files:
 
-with open(args.input,'r') as file:
-	encrypted = file.read()
-encryption = encrypted.split(':')[0]
-to_encrypt = encrypted.split(':')[1]
-if encryption == "Hex":
-	decrypted = decryptHex(to_encrypt).lower()
-elif encryption == "Caesar Cipher(+3)":
-	decrypted = decryptCaesar(to_encrypt).lower()
-elif encryption == "Morse Code":
-	decrypted = decryptMorse(to_encrypt).lower()
+	with open(path_start + i,'r') as file:
+		encrypted = file.read()
+	encryption = encrypted.split(':')[0]
+	to_encrypt = encrypted.split(':')[1]
+	if encryption == "Hex":
+		decrypted = decryptHex(to_encrypt).lower()
+	elif encryption == "Caesar Cipher(+3)":
+		decrypted = decryptCaesar(to_encrypt).lower()
+	elif encryption == "Morse Code":
+		decrypted = decryptMorse(to_encrypt).lower()
 
-with open(args.output,'w') as file:
-	file.write(decrypted)
+	try:
+		if args.output[0] != "/":
+			out_file = open(args.output + "/" + i[:len(i)-4] + "_e35918rs.txt",'w')
+		else:
+			out_file = open(args.output + i[:len(i)-4] + "_e35918rs.txt",'w')
+		out_file.write(decrypted)
+		out_file.close()
+	except:
+		os.mkdir(args.output)
+		if args.output[0] != "/":
+			out_file = open(args.output + "/" + i[:len(i)-4] + "_e35918rs.txt",'w')
+		else:
+			out_file = open(args.output + i[:len(i)-4] + "_e35918rs.txt",'w')
+		out_file.write(decrypted)
+		out_file.close()
